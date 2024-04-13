a11e1388e70f80025886cb7492d9dd540f2e10ba
Sergey Plashkin <sergey.plashkin@mail.ru>
2024-04-13 14:21:37 +0300

    Изменение настроек

--- CinemaBack/obj/Debug/net8.0/CinemaBack.AssemblyInfo.cs
+++ CinemaBack/obj/Debug/net8.0/CinemaBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d15b1194e663cbba2f770ef7a339f2ebb30b87e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
