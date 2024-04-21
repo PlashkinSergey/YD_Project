7a35c160a0d896ba0a9e980fd9af738b0768b07c
Sergey Plashkin <sergey.plashkin@mail.ru>
2024-04-21 19:29:34 +0300

    Новый запуск сервера

--- CinemaBack/Hash/Hasher.cs
+++ CinemaBack/Hash/Hasher.cs
@@ -6,16 +6,17 @@ namespace CinemaBack.Hash
     public class Hasher
     {
         private readonly string _password;
+        private readonly MD5 _md5;
         public Hasher(string password) 
         {
             this._password = password;
+            _md5 = MD5.Create();
         }
 
         public string GetHash()
         {
-            MD5 md5 = MD5.Create();
             byte[] bytes = Encoding.ASCII.GetBytes(_password);
-            byte[] hashs = md5.ComputeHash(bytes);
+            byte[] hashs = _md5.ComputeHash(bytes);
             StringBuilder sb = new StringBuilder();
             foreach (byte hash in hashs)
             {
